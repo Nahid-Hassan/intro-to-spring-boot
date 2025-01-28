c9541aa98e5724bb63dcde73f627da3ca549faad
Nahid-Hassan <nahidmdh18@gmail.com>
2025-01-28 21:07:37 +0600

    Add tic-tac-toe board ts

--- day-02-tic-tac-toe/tic-tac-toe/src/components/Square.tsx
+++ day-02-tic-tac-toe/tic-tac-toe/src/components/Square.tsx
@@ -1,25 +1,29 @@
-function Square() {
-    return (
-        <button className="square">1</button>
-    )
+interface SquareProps {
+    value: string;
 }
 
-function TicTacToeBoardRow() {
-    return (
-        <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-        </div>
-    )
+function Square({ value }: SquareProps) {
+    return <button className="square">{value}</button>;
 }
 
-export default function TicTocToeBoard() {
+export default function Board() {
     return (
-        <div>
-            <TicTacToeBoardRow />
-            <TicTacToeBoardRow />
-            <TicTacToeBoardRow />
-        </div>
-    )
+        <>
+            <div className="board-row">
+                <Square value="1" />
+                <Square value="2" />
+                <Square value="3" />
+            </div>
+            <div className="board-row">
+                <Square value="4" />
+                <Square value="5" />
+                <Square value="6" />
+            </div>
+            <div className="board-row">
+                <Square value="7" />
+                <Square value="8" />
+                <Square value="9" />
+            </div>
+        </>
+    );
 }
